39c1a4122c9fcd7d7a231c229e5fe27cb6a659e1
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-02-14 00:26:41 +0900

    Add run_all_repos mode

---
+++ README.md
@@ -0,0 +1,20 @@
+RuboCop Regression Test
+====
+
+
+
+Usage
+---
+
+```bash
+# Run on many repositories, all configuration conbinations
+$ ruby main.rb
+
+# Run on specified repo, default configuration
+$ ruby main.rb owner/repo
+
+# Run on specified repo, and specified config
+$ CONFIGS='path/to/config.yml path/to/config2.yml' main.rb owner/repo
+```
+
+
--- main.rb
+++ main.rb
@@ -1,12 +1,37 @@
 require_relative 'lib/runner'
 
 def main(argv)
+  if argv.empty?
+    run_all_repos
+  else
+    run_one_repo(argv)
+  end
+end
+
+TARGET_REPOSITORIES = %w[
+  tric/trick2013
+  tric/trick2015
+  tric/trick2018
+  ruby/ruby
+  ruby/spec
+  rails/rails
+  gitlabhq/gitlabhq
+  discourse/discourse
+]
+
+def run_one_repo(argv)
   raise "unexpected arguments" if argv.size != 1
 
   configs = (ENV['CONFIGS'] && ENV['CONFIGS'].split(' ')) || [:force_default_config]
 
   runner = Runner.new(argv.first, configs: configs)
   runner.run
+end
+
+def run_all_repos
+  TARGET_REPOSITORIES.each do |repo|
+    Runner.new(repo, configs: [:force_default_config]).run
+  end
 end
 
 main(ARGV)
