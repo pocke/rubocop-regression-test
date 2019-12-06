9efd9247888eccdca32f080fa1a020061a7cb87a
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-12-06 23:26:00 +0900

    Refactor for preparing generate config

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,20 +14,35 @@ class CLI
   end
 
   def start
+
+    usage! unless subcommand
+
+    case subcommand
+    when 'check'
+      check
+    when 'generate_config'
+      generate_config
+    when 'help'
+      usage!
+    end
+  end
+
+  attr_reader :argv, :error_queue, :debug
+  private :argv, :error_queue, :debug
+
+  private def check
     th = watch_error_queue
     CircleCI.start
-
-    raise "Usage: ruby main.rb owner/repo" if argv.empty?
-
-    run_for(argv.first)
+    run_for(argv[1])
 
     error_queue.close
     th.join
     exit @exit_status
   end
 
-  attr_reader :argv, :error_queue, :debug
-  private :argv, :error_queue, :debug
+  private def generate_config
+    raise 'WIP'
+  end
 
   private def run_for(repo)
     Runner.new(repo, configs: configs, error_queue: error_queue, debug: debug).run
@@ -44,5 +59,19 @@ class CLI
         @exit_status = EXIT_STATUS_ERR
       end
     end
+  end
+
+  private def subcommand
+    @argv[0]
+  end
+
+  private def usage!
+    puts <<~USAGE
+      Usage:
+        ruby main.rb check OWNER/REPO       # Run the test
+        ruby main.rb generate_config NUMBER # Generate configuration
+        ruby main.rb help                   # Display this message
+    USAGE
+    exit EXIT_STATUS_ERR
   end
 end
